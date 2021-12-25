68a397c31f8e21913269dca60f464efea38d1771
Mitchell Zehr <zehrmitch@gmail.com>
2021-12-25 16:39:27 -0500

    Update ProtectedRoute.js

--- src/auth/ProtectedRoute.js
+++ src/auth/ProtectedRoute.js
@@ -3,7 +3,6 @@ import { Route, Navigate } from 'react-router-dom';
 import { withAuthenticationRequired } from '@auth0/auth0-react';
 import Loading from '../components/Loading';
 import { useAuth0 } from '@auth0/auth0-react';
-import LandingPage from '../pages/LandingPage';
 
 const ProtectedRoute = ({ component, ...args }) => {
 	const { isAuthenticated } = useAuth0();
